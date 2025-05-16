d9393ed313215f038de3fa2742a74b3152341b9d
Justin <justin.turner245@gmail.com>
2025-05-16 15:16:06 -0400

    Update timers.c
    Fixed typos and corrected some descriptions

--- timers.c
+++ timers.c
@@ -21,7 +21,6 @@ void configureSysTickInterrupt(void)
 {
 	SysTick->CTRL = 0; //disable SysTick timer
 	NVIC_SetPriority(SysTick_IRQn, 7); //set priority level at 7
-	//SysTick->LOAD = FLASHfrequency[speeds]; //set the counter reload value
 	SysTick->LOAD = 3999; //set the counter reload value 1ms
 	SysTick->VAL = 0; //reset SysTick timer value
 	SysTick->CTRL |= SysTick_CTRL_CLKSOURCE_Msk; //use system clock
@@ -34,20 +33,18 @@ void configureSysTickInterrupt(void)
 // @parm: none
 // @return: none
 //
-// 	Recalculates and sets TIM2's ARR register based on the specified speed. This directly
-//	affects how often the timer overflows and triggers an interrupt. The timer counter (CNT)
-//	is also reset to ensure immediate effect.
+// 	Configures TIM2 to for interrupt generation
 //
 //       Note: The counter reset and ARR value is handled by the updateARR function
 //	       The Timer does not start until the INITIAL_SERVE state.
 //================================================================================================
 void configureTIM2 (void)
 {
-		 RCC->APB1ENR1 |= (1 << 0);  // Enable TIM2 clock
-	  	 TIM2->PSC = (SYS_CLK_FREQ/cntclk -1);
-	  	 TIM2->DIER |= (1 << 0);          // Enable update interrupt
-	     NVIC_SetPriority(TIM2_IRQn, 2); //set priority level at 2
-	     NVIC_EnableIRQ(TIM2_IRQn);       // Enable interrupt in NVIC
+	  RCC->APB1ENR1 |= (1 << 0);  // Enable TIM2 clock
+	  TIM2->PSC = (SYS_CLK_FREQ/cntclk -1);
+	  TIM2->DIER |= (1 << 0);          // Enable update interrupt
+	  NVIC_SetPriority(TIM2_IRQn, 2); //set priority level at 2
+	  NVIC_EnableIRQ(TIM2_IRQn);       // Enable interrupt in NVIC
 }
 
 //================================================================================================
@@ -56,9 +53,9 @@ void configureTIM2 (void)
 // @parm: speed = Desired update frequency (in HZ)
 // @return: none
 //
-// 		 Recalculates and sets TIM2's ARR register based on the specified speed. This directly
-//		 affects how often the timer overflows and triggers an interrupt. The timer counter (CNT)
-//		 is also reset to ensure immediate effect.
+// 	Recalculates and sets TIM2's ARR register based on the specified speed. This directly
+//	affects how often the timer overflows and triggers an interrupt. The timer counter (CNT)
+//	is also reset to ensure immediate effect.
 //================================================================================================
 void updateARR (uint32_t speed)
 {
